f072043ec50770dc20359806520cf50b0478cda1
CHAIBOU MAKADA MADO Djibril <djibrilchaibou.dc@gmail.com>
2025-10-15 16:55:01 +0100

    Init project

---
+++ src/main/java/com/taramtech/taram_pass/TaramPassApplication.java
@@ -0,0 +1,13 @@
+package com.taramtech.taram_pass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaramPassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaramPassApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/taramtech/taram_pass/TaramPassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taramtech.taram_pass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaramPassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
